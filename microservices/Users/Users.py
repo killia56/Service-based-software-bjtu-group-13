be4520af01039c7e77cb6c9ebad4e9d296d23cb0
killia56 <killia@hotmail.fr>
2020-04-23 00:09:52 +0200

    My work

--- microservices/Users/Users.py
+++ microservices/Users/Users.py
@@ -1,19 +1,23 @@
 import flask as fl
 import sqlite3
 import requests
-conn = sqlite3.connect('Users.db')
-c = conn.cursor()
 app = fl.Flask(__name__)
 
 def createUser(t):
+	conn = sqlite3.connect('Users.db')
+	c = conn.cursor()
 	c.execute("INSERT INTO users VALUES (?,?)", t)
 	conn.commit()
 
 def loginUser(t):
+	conn = sqlite3.connect('Users.db')
+	c = conn.cursor()
 	c.execute('SELECT * FROM users WHERE login=? AND password=?', t)
 	return c.fetchone()!=None
 
 def searchUser(t):
+	conn = sqlite3.connect('Users.db')
+	c = conn.cursor()
 	c.execute('SELECT * FROM users WHERE login=?', t)
 	return c.fetchone()!=None
 
@@ -21,17 +25,22 @@ def searchUser(t):
 def index():
 	return "U"
 
-@app.route('/signin', methods=['GET', 'POST'])
-def signin():
+@app.route('/signin/<username>/<password>', methods=['GET', 'POST'])
+def signin(username, password):
+	if searchUser((username,)) == False:
+		createUser((username, password,))
 	rep = str(fl.request.data)
-	return True
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	rep = str(fl.request.data)
-	print("lol",rep)
 	return "True"
+
+@app.route('/login/<username>/<password>', methods=['GET', 'POST'])
+def login(username, password):
+	rep = str(fl.request.data)
+	res = loginUser((username, password,))
+	print(res)
+	return "OK"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5001, debug=True)
+
+conn.close()
 	
